7dda2ca3ac18f65e48031aa6ca7a3605f0293151
Parthjhaveri <parthjhaveri209@gmail.com>
2021-04-11 19:49:35 -0400

    added babel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './styles/styles.css';
 
-import { color_red } from './app.js';
+import { color_red, my_cars } from './app.js';
 import { color_green } from './app_two.js';
 import { color_blue } from './app_three.js';
 import { color_brown } from './app_four.js';
+
+// TEST ES6 CODE
+const car = 'Aston Martin';
+
+const return_car = () => {
+	return 'My favorite car is an ' + car;
+}
+
+return_car();
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ module.exports = {
 			{
 				test: /\.css$/,
 				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"]
 			}
 		]
 	}
